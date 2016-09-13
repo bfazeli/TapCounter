ec660850c9c5282f6ae7591fd3f878c889e3ee62
bijan102 <bfazeli1@gmail.com>
2016-09-12 23:42:57 -0700

    First commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/Counter.java
@@ -0,0 +1,16 @@
+package edu.orangecoastcollege.cs273.bfazeli.tapcounter;
+
+/**
+ * Created by bijanfazeli on 9/12/16.
+ */
+public class Counter {
+    private int mCount;
+
+    public int getCount() {
+        return mCount;
+    }
+
+    public void incrementCount() {
+        mCount++;
+    }
+}
