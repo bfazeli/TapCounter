f7e567be01a764cb70f1c28ebafc117f2721ef50
bijan102 <bfazeli1@gmail.com>
2016-09-12 23:59:55 -0700

    Connected model, view, and controller together.

--- app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/Counter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/Counter.java
@@ -6,10 +6,18 @@ package edu.orangecoastcollege.cs273.bfazeli.tapcounter;
 public class Counter {
     private int mCount;
 
+    /**
+     * getCount:    Returns the count variable.
+     *
+     * @return:     Returns the current count variable when called.
+     */
     public int getCount() {
         return mCount;
     }
 
+    /**
+     * incrementCount:  Increments the count variable by one when called
+     */
     public void incrementCount() {
         mCount++;
     }
--- app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/MainActivity.java
@@ -2,12 +2,33 @@ package edu.orangecoastcollege.cs273.bfazeli.tapcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Associate the controller with the needed model
+    Counter num = new Counter();
+
+    Button tapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num.getCount();              //Display the current count when app loads, 0.
+
+
+        // Hook up the tapButton with the one from the View
+        tapButton = (Button) findViewById(R.id.tapButton);
+
+        // Set up an ActionListener for when the button is clicked
+        tapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num.incrementCount();
+            }
+        });
     }
 }
