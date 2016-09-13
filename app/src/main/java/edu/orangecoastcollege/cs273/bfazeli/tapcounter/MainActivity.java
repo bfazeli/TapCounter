cff8ed9a2482449ac6c8818c898bba2b4010716b
bijan102 <bfazeli1@gmail.com>
2016-09-13 00:21:35 -0700

    Completed Tap Counter app

--- app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/tapcounter/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,23 +12,25 @@ public class MainActivity extends AppCompatActivity {
     Counter num = new Counter();
 
     Button tapButton;
+    TextView countTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num.getCount();              //Display the current count when app loads, 0.
-
-
-        // Hook up the tapButton with the one from the View
+        // Hook up the tapButton and countTextView with the one from the View
         tapButton = (Button) findViewById(R.id.tapButton);
+        countTextView = (TextView) findViewById(R.id.countTextView);
+
+        countTextView.setText(Integer.toString(num.getCount()));        //Display the current count when app loads, 0.
 
         // Set up an ActionListener for when the button is clicked
         tapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num.incrementCount();
+                num.incrementCount();                        // Increment the count
+                countTextView.setText(Integer.toString(num.getCount()));       // Display the updated count
             }
         });
     }
